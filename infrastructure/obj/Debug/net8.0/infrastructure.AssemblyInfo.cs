8a7cb81833c3a55ed6ceed0f4e11fd7f33af23e6
Ionut Andrei Grosu <80783004+grosuionutandrei@users.noreply.github.com>
2024-10-09 22:17:15 +0200

    unit tests and integration tests implemented

--- infrastructure/obj/Debug/net8.0/infrastructure.AssemblyInfo.cs
+++ infrastructure/obj/Debug/net8.0/infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9676c35291d12c13e857633285a71a1371657afd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a86b4b48c570674ad8c77832e9d0386bfe125ca7")]
 [assembly: System.Reflection.AssemblyProductAttribute("infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- service/obj/Debug/net8.0/service.AssemblyInfo.cs
+++ service/obj/Debug/net8.0/service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9676c35291d12c13e857633285a71a1371657afd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a86b4b48c570674ad8c77832e9d0386bfe125ca7")]
 [assembly: System.Reflection.AssemblyProductAttribute("service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
