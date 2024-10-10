981a43fe75c7773ac70fc877e2e4ae50a2592733
Ionut Andrei Grosu <80783004+grosuionutandrei@users.noreply.github.com>
2024-10-10 13:27:41 +0200

    changed the yaml

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62927a8f92e4604fa03891b485f131351eaa2b6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e9562f1e5bf39918e75b8ad2f2261deaf616b12")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- test/obj/Debug/net8.0/test.AssemblyInfo.cs
+++ test/obj/Debug/net8.0/test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a7cb81833c3a55ed6ceed0f4e11fd7f33af23e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e9562f1e5bf39918e75b8ad2f2261deaf616b12")]
 [assembly: System.Reflection.AssemblyProductAttribute("test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
