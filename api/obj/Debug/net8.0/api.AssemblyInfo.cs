9e8b3bc098a154af91cfeff22f66b48202b5f4f7
Ionut Andrei Grosu <80783004+grosuionutandrei@users.noreply.github.com>
2024-10-10 13:46:15 +0200

    fixed the utilities

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5a5006d804757b894c1ac32860ee0fa690e6b17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8c65db7c57988f65d2731dfee8dc8ee65e9e270")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
